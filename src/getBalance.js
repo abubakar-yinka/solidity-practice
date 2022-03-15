83cf7a726a1c00858aef880d0beca7dddbedeb86
Abubakar Ibrahim <abubakaryinka75@gmail.com>
2022-03-15 02:46:53 +0100

    Refactor

--- src/getBalance.js
+++ src/getBalance.js
@@ -6,7 +6,7 @@ const provider = require("./provider");
 async function main() {
   const account = wallet.connect(provider);
   const balance = await account.getBalance();
-  console.log(`ETH Balance: ${ethers.utils.formatUnits(balance, 18)}`);
+  console.log(`ETH Balance: ${ethers.utils.formatEther(balance)}`);
 }
 
 main();
