68cc676d6247d9bc23a16d24ba11bd1ed3fb0f56
Abubakar Ibrahim <abubakaryinka75@gmail.com>
2022-03-15 03:41:26 +0100

    Modify getBalance func to call a smart contract func to read data(USDC)

--- src/getBalance.js
+++ src/getBalance.js
@@ -5,8 +5,23 @@ const provider = require("./provider");
 
 async function main() {
   const account = wallet.connect(provider);
-  const balance = await account.getBalance();
-  console.log(`ETH Balance: ${ethers.utils.formatEther(balance)}`);
+
+  // Define contract interface
+  const usdc = new ethers.Contract(
+    "0x68ec573C119826db2eaEA1Efbfc2970cDaC869c4",
+    [
+      "function balanceOf(address _owner) public view returns (uint256 balance)",
+    ],
+    account,
+  );
+
+  const ethBalance = await account.getBalance();
+  console.log(`ETH Balance: ${ethers.utils.formatEther(ethBalance)}`);
+
+  // Call balanceOf function, a read-only function, so it can be called for free and does not need to be submitted as a transaction
+  const usdcBalance = await usdc.balanceOf(account.address);
+  // USDC uses 6 decimal places of precision as opposed to 18 that many other ERC20 tokens use.
+  console.log(`USDC Balance: ${ethers.utils.formatUnits(usdcBalance, 6)}`);
 }
 
 main();
